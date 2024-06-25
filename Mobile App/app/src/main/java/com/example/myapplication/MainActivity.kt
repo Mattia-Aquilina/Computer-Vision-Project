aabe39f51c667f1dde46c979caa76268112e1695
Mattia Aquilina <mattia.eagle@gmail.com>
2024-06-25 11:50:57 +0200

    Detection and Generation windows

--- Mobile App/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Mobile App/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,13 +10,20 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Button
@@ -26,6 +33,8 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Camera
 import androidx.compose.material.icons.filled.GeneratingTokens
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,7 +43,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -126,22 +138,164 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Generation() {}
+fun Generation() {
+    var prompt by remember { mutableStateOf<String>("") }
+    var steps by remember { mutableStateOf<String>("20") }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Generation",
+            style = MaterialTheme.typography.h5.copy(
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp
+            ),
+            color = Color.Black,
+        )
+
+
+            //Result displayer
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .fillMaxHeight(.4f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+
+                OutlinedTextField(
+                    label = { Text(text = "Prompt") },
+                    placeholder = { Text(text = "Insert Prompt") },
+                    value = prompt,
+                    minLines = 3,
+                    maxLines = 3,
+                    onValueChange = { newText: String ->
+                        prompt = newText
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                OutlinedTextField(
+                    value = steps,
+                    label = { Text(text = "Steps") },
+                    placeholder = { Text(text = "Insert steps") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    onValueChange = { it ->
+                        steps = it
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Button(
+
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+                ) {
+                    Text("Generate")
+                }
+        }
+
+        if(imageUri != null) {
+            Image(
+                painter = rememberAsyncImagePainter(imageUri),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.7f)
+            )
+        } else {
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.7f)
+                    .border(width = 2.dp, color = Color.Gray)
+                ,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Image,
+                    contentDescription = "Generation"
+                )
+                Text(text = "Result will appear here")
+
+            }
+        }
+    }
+
+}
+
 
 
 @Composable
 fun Detection() {
     var pictureUri by remember { mutableStateOf<Uri?>(null) }
 
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .fillMaxHeight()
             .padding(16.dp)
     ) {
-        Text(text = "Detection", Modifier.size(20.dp))
+        Text(
+            text = "Detection",
+            style = MaterialTheme.typography.h5.copy(
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp
+            ),
+            color = Color.Black,
+            )
+
+
         ImageUploadButton(onImageSelected = { uri ->
             pictureUri = uri
         })
+
+        Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(.7f)
+            .border(width = 2.dp, color = Color.Gray)
+            .padding(20.dp)
+        ) {
+            //Result displayer
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .fillMaxHeight(.7f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+
+                Text(text = "Results will display here")
+                Text(text = "Fake/real")
+                Text(text = "Time taken:")
+            }
+
+            Button(
+                enabled = pictureUri != null,
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text("Check")
+            }
+
+
+        }
     }
 
     // Add a way to upload a picture
@@ -150,7 +304,7 @@ fun Detection() {
 
 
 @Composable
-fun ImageUploadButton(onImageSelected: (Uri) -> Unit) {
+fun ImageUploadButton(onImageSelected: (Uri?) -> Unit) {
     var imageUri by remember { mutableStateOf<Uri?>(null) }
 
     // Create an activity result launcher for the image picker
@@ -164,7 +318,7 @@ fun ImageUploadButton(onImageSelected: (Uri) -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .fillMaxHeight(.5f)
     ) {
         // Display selected image
         if (imageUri != null) {
@@ -173,21 +327,77 @@ fun ImageUploadButton(onImageSelected: (Uri) -> Unit) {
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp)
+                    .fillMaxHeight(0.7f)
             )
+
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp),
+                horizontalArrangement = Arrangement.Center
+
+            ) {
+                Button(
+                    onClick = {
+                        launcher.launch("image/*")
+                    },
+                    modifier = Modifier
+                        .width(150.dp)
+                        .padding(top = 16.dp)
+                ) {
+                    Text("Change", maxLines = 1, style = MaterialTheme.typography.h5.copy(fontSize = 13.sp),)
+                }
+
+                Spacer(Modifier.width(20.dp))
+
+                Button(
+                    onClick = {
+                        onImageSelected(null);
+                        imageUri = null;
+                    },
+                    modifier = Modifier
+                        .width(150.dp)
+                        .padding(top = 16.dp)
+                ) {
+                    Text("Remove", maxLines = 1, style = MaterialTheme.typography.h5.copy(fontSize = 13.sp),)
+                }
+            }
+
+        }else
+        {
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.7f)
+                    .border(width = 2.dp, color = Color.Gray)
+                    .clickable(onClick = { launcher.launch("image/*"); }),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Image,
+                    contentDescription = "Generation"
+                )
+                Text(text = "Load an image to procede")
+
+            }
+
+            Button(
+                onClick = {
+                    launcher.launch("image/*")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+
+                    .padding(top = 16.dp)
+            ) {
+                Text("Select Image")
+            }
+
         }
 
         // Button to launch the image picker
-        Button(
-            onClick = {
-                launcher.launch("image/*")
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp)
-        ) {
-            Text("Select Image")
-        }
+
     }
 }
 
